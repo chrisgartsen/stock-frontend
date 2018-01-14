740356a000b1893da78d2a2f682a1a7e78dd29ab
Chris Gartsen <github@gartsen.nl>
2018-01-14 20:18:02 +0100

    Initial commit

---
+++ src/components/main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Testing
+    <br/>
+    <button v-on:click="fetchAll">Fetch All</button>
+    <button v-on:click="addOne">AddOne</button>
+  </div>
+</template>
+
+<script>
+import Axios from 'axios'
+
+export default {
+  methods: {
+    fetchAll: function () {
+      console.log("Getting all items")
+      Axios.get("http://localhost:3000/items")
+        .then((response) => {
+          console.log(response.data)
+        })
+    },
+    addOne() {
+      console.log("Adding item")
+      Axios.post("http://localhost:3000/items", {name: 'Oww', quantity: 1, minimum_quantity: 3})
+    }
+  }
+}
+</script>
