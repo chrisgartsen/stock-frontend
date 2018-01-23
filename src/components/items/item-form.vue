fad21ed525cdf38884e15e5373f5a29592841d09
Chris Gartsen <github@gartsen.nl>
2018-01-23 22:23:26 +0100

    Template for quantity component

--- src/components/items/item-form.vue
+++ src/components/items/item-form.vue
@@ -28,29 +28,12 @@
 
           <div class="field is-horizontal">
             <div class="field-label">
-              <label for="quantity" class="label">Quantity *</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control">
-                  <input type="text" id="quantity" class="input is-small-width" :class="{'is-danger': $v.quantity.$error}" v-model="quantity" @input="$v.quantity.$touch()">
-                </div>
-                <ul v-if="$v.quantity.$error" class="help is-danger">
-                  <li v-if="!$v.quantity.required">Quantity is required</li>
-                  <li v-if="!$v.quantity.numeric">Quantity must be a number</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div class="field is-horizontal">
-            <div class="field-label">
               <label for="minimum_quantity" class="label">Minimum quantity *</label>
             </div>
             <div class="field-body">
               <div class="field">
                 <div class="control">
-                  <input type="text" id="minimum_quantity" class="input is-small-width" :class="{'is-danger': $v.minimum_quantity.$error}" v-model="minimum_quantity" @input="$v.minimum_quantity.$touch()">
+                  <input type="text" id="minimum_quantity" class="input is-small-number" :class="{'is-danger': $v.minimum_quantity.$error}" v-model="minimum_quantity" @input="$v.minimum_quantity.$touch()">
                 </div>
                 <ul v-if="$v.minimum_quantity.$error" class="help is-danger">
                   <li v-if="!$v.minimum_quantity.required">Minimum quantity is required</li>
@@ -59,6 +42,9 @@
               </div>
             </div>
           </div>
+
+          <quantity-inputfield fieldName="quantity" fieldLabel = "Quantity *" :value="quantity" @changeValue="updateQuantity"/>
+
           <span class="help">Fields marked with * are required.</span>
         </form>
 
@@ -75,6 +61,8 @@
 <script>
 import { required, numeric } from 'vuelidate/lib/validators'
 
+import quantityInputfield from './quantity-inputfield'
+
 export default {
   name: 'item-form',
   props: {
@@ -83,8 +71,8 @@ export default {
       type: Boolean
     }
   },
-  computed: {
-
+  components: {
+    quantityInputfield
   },
   data() {
     return {
@@ -121,6 +109,9 @@ export default {
         this.$store.dispatch('CREATE_ITEM', {name: this.name, quantity: this.quantity, minimum_quantity: this.minimum_quantity})
         this.closeForm()
       }
+    },
+    updateQuantity(newValue) {
+      this.quantity = newValue
     }
   }
 }
---
+++ src/components/items/quantity-inputfield.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="field is-horizontal">
+    <div class="field-label">
+      <label :for="fieldName" class="label">{{ fieldLabel }}</label>
+    </div>
+    <div class="field-body">
+      <div class="field has-addons">
+        <div class="control">
+          <a class="button is-primary" @click="increment"><i class="fa fa-plus"></i></a>
+        </div>
+        <div class="control">
+          <input type="text" :id="fieldName" class="input is-small-number" v-model="value">
+        </div>
+        <div class="control">
+          <a class="button is-primary" @click="decrement"><i class="fa fa-minus"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'quantity-inputfield',
+  props: {
+    fieldName: {
+      type: String,
+      required: true
+    },
+    fieldLabel: {
+      type: String,
+      default: this.fieldName
+    },
+    value: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    increment() {
+      this.$emit("changeValue", this.value+1)
+    },
+    decrement() {
+      this.$emit("changeValue", this.value-1)
+    }
+  }
+}
+</script>
