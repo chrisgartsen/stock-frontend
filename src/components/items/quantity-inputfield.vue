1b87bed4ce59d48bf866bb9c3011b53d7ccca7b2
Chris Gartsen <github@gartsen.nl>
2018-01-25 20:35:42 +0100

    Add internal data object to quantity component

--- src/components/items/quantity-inputfield.vue
+++ src/components/items/quantity-inputfield.vue
@@ -9,7 +9,7 @@
           <a class="button is-primary" @click="increment"><i class="fa fa-plus"></i></a>
         </div>
         <div class="control">
-          <input type="text" :id="fieldName" class="input is-small-number" v-model="value" @change="change">
+          <input type="text" :id="fieldName" class="input is-small-number" v-model="val" @change="change">
         </div>
         <div class="control">
           <a class="button is-primary" @click="decrement"><i class="fa fa-minus"></i></a>
@@ -32,19 +32,32 @@ export default {
       default: this.fieldName
     },
     value: {
-      type: Number,
       required: true
     },
   },
+  validate: {
+
+  },
+  
+  data() {
+    return {
+      val : this.value
+    }
+  },
   methods: {
     increment() {
-      this.$emit("changeValue", this.value+1)
+      this.$emit("changeValue", this.val+1)
     },
     decrement() {
-      this.$emit("changeValue", this.value-1)
+      this.$emit("changeValue", this.val-1)
     },
     change() {
-      console.log(this.value)
+      this.$emit("changeValue", this.val)
+    }
+  },
+  watch: {
+    value(newVal) {
+      this.val = newVal
     }
   }
 }
