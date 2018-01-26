1a108ea91480acc38485b0c8cc599abf091a4fd9
Chris Gartsen <github@gartsen.nl>
2018-01-26 16:51:15 +0100

    Put button icon in component

---
+++ src/components/items/icon-button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="button is-primary" @click="buttonClick">
+    <span><i class="fa fa-plus"></i></span >
+    <span>{{ label }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'icon-button',
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    buttonClick() {
+      this.$emit("buttonClick")
+    }
+  }
+}
+</script>
