39fbebb93aa1134618490a30edb3df94cf502c3a
Chris Gartsen <github@gartsen.nl>
2018-02-08 19:39:01 +0100

    Add Element-UI to app

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-// Bulma
-import 'font-awesome/css/font-awesome.css'
-import './assets/sass/main.scss'
+// Element UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 // Utility libaries
 import Vuelidate from 'vuelidate'
