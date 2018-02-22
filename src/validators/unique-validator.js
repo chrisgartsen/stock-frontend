18c56c744a7817d736a0729cacc7bf76e4e854b0
Chris Gartsen <github@gartsen.nl>
2018-02-22 20:37:11 +0100

    Bugfix in uniqueness validator

--- src/validators/unique-validator.js
+++ src/validators/unique-validator.js
@@ -5,6 +5,6 @@ export default withParams({ type: 'unique' }, (email) => {
     if(email === '') return true // do not check empty values for uniqueness
     let users = store.getters.getUsers
     
-    let index = checkList.map(function(user) { return user.email}).indexOf(email)
+    let index = users.map(function(user) { return user.email}).indexOf(email)
     return index == -1
   })
