24a03a51616090c06ba3933a2f533981a7aeca2c
Chris Gartsen <github@gartsen.nl>
2018-03-02 20:56:39 +0100

    Add router guard

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,26 @@ import users from '@/components/users/users-page'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   base: '/',
   mode: 'history',
   linkActiveClass: 'is-active',
   routes: [
     { path: '/', name: 'items', component: items },
-    { path: '/users', name: 'users', component: users}
-  ]
+    { path: '/users', name: 'users', component: users, meta: { requiresAuth: true }}
+  ],
+
 })
+
+router.beforeEach((to, from, next) =>{
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    console.log("NAV - AUTH REQUIRED")
+    next({
+      path: '/'
+    })
+  } else {
+    next()
+  }
+})
+
+export default router
